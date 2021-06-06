d8ed504b5f25583e2bd754d1e6db2731b54b8aa6
dongcheolpark <head02325@soongsil.ac.kr>
2021-06-06 16:55:15 +0900

    a

--- commands/team.js
+++ commands/team.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "대학팀",
     execute(msg,args){
-        let role = msg.guild.roles.cache.find(r => r.name === args[1]);
+        let role = msg.guild.roles.cache.find(r => r.name === args[0]);
         msg.member.roles.add(role);
         msg.member.setNickname(`[인증된] ${msg.member.user.username}`)
         msg.reply("성공적으로 역할을 지급하고 별명을 변경하였습니다.")
