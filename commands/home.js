54591ee46ca1bb467ac66935056c292168e1efb3
bigi0921 <32163765+bigi0921@users.noreply.github.com>
2021-06-15 17:21:01 +0900

    fdd

--- commands/home.js
+++ commands/home.js
@@ -7,7 +7,7 @@ module.exports = {
             .setColor('#87CEEB')
             .setTitle('대회봇 안내')
             .addFields(
-                {name:"카페",value : "대회에 참여하기위해서는 카페가입과 본인명의 인증을 해야합니다. \n\ :arrow_forward: 네이버 카페 : https://cafe.naver.com/lof0525 \n\ \n\ :arrow_forward: 디스코드 서버 주소 : https://discord.gg/2jFZvSna "},
+                {name:"카페",value : "대회에 참여하기위해서는 카페가입과 본인명의 인증을 해야합니다. \n\ :arrow_forward: 네이버 카페 : https://cafe.naver.com/lof0525 \n\ \n\ :arrow_forward: 디스코드 서버 주소 : https://discord.gg/h5HECqXcGT "},
             );
         msg.channel.send(embmsg);
     }
